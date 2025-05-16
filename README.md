0efb1def50e79453ccbd7e232633795d357f5743
Toon Van de Maele <toonvandemaele@gmail.com>
2025-05-16 10:24:43 +0200

    added licenses

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # Variational Bayes Gaussian Splatting
 
-This repository contains code accompanying the paper [Variational Bayes Gaussian Splatting](https://arxiv.org/abs/2410.03592
-) by Toon Van de Maele, Ozan Catal, Alexander Tschantz, Christopher L. Buckley, and Tim Verbelen. 
-
 ## Installation 
 
 The package for optimizing VBGS can be installed using the following command in the root folder: 
@@ -62,22 +59,6 @@ Running the Habitat benchmark and continual benchmark is similar to the object c
 .
 ```
 python scripts/train_rooms.py
-```
-
-## BibTeX
-
-
-If you find this repository helpful, please consider citing our work: 
-```
-@misc{vandemaele2024variationalbayesgaussiansplatting,
-      title={Variational Bayes Gaussian Splatting}, 
-      author={Toon Van de Maele and Ozan Catal and Alexander Tschantz and Christopher L. Buckley and Tim Verbelen},
-      year={2024},
-      eprint={2410.03592},
-      archivePrefix={arXiv},
-      primaryClass={cs.CV},
-      url={https://arxiv.org/abs/2410.03592}, 
-}
 ```
 
 ## License
--- scripts/convert_models.py
+++ scripts/convert_models.py
@@ -1,3 +1,18 @@
+# Copyright 2024 VERSES AI, Inc.
+#
+# Licensed under the VERSES Academic Research License (the “License”);
+# you may not use this file except in compliance with the license.
+#
+# You may obtain a copy of the License at
+#
+#     https://github.com/VersesTech/vbgs/blob/main/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pickle
 import json
 import jax.numpy as jnp
@@ -9,6 +24,7 @@ import tqdm
 import numpy as np
 
 if __name__ == "__main__":
+    # Converts models from json to npz (less storage required)
     p = "/home/toon.vandemaele/projects/iclr-rebuttal/vbgs-internal/scripts/data/sweep/"
     p = Path(p)
 
@@ -29,5 +45,5 @@ if __name__ == "__main__":
         with open(pa, "wb") as fp:
             np.savez(fp, mu=mu, si=si, alpha=alpha)
 
-        del d 
+        del d
         os.remove(str(f))
